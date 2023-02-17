fabc3cee3f4b46a708d57a117b5c9f9df14d2dee
kharleanncf <kharleann@gmail.com>
2023-02-16 22:00:34 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,6 @@ module.exports = {
 		filename: "[name].bundle.js",
 		path: path.resolve(__dirname, "dist"),
 		clean: true,
-		publicPath: "/public/",
 	},
 	optimization: {
 		runtimeChunk: "single",
